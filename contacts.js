bdff960eca07f7630c16d86ddadd78c2f9ec8a86
Krzysztof R <plkrozbicki@gmail.com>
2023-08-10 17:51:19 +0100

    Added edit feature with readline and try / catch block for all functions

--- contacts.js
+++ contacts.js
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import { readFile, writeFile } from 'node:fs/promises';
 import { nanoid } from 'nanoid';
+import * as readline from 'readline';
 
 const contactsPath = path.resolve('db', 'contacts.json');
 const backupContactsPath = path.resolve('backup', 'contacts.json');
@@ -26,45 +27,114 @@ const writeContactsFile = async data => {
 };
 
 export const listContacts = async () => {
-  const contacts = await readContactsFile();
-  if (contacts) return contacts;
-  return false;
+  try {
+    const contacts = await readContactsFile();
+    if (contacts) return contacts;
+    return false;
+  } catch (err) {
+    console.log('Error getting contact list: ', err);
+    throw new Error(err);
+  }
 };
 
 export const getContactById = async contactId => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex(contact => contact.id === contactId);
-  if (index === -1 && contacts) {
-    console.log(`There is no contact with id ${contactId} !`);
-    return false;
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    if (index === -1 && contacts) {
+      console.log(`There is no contact with id ${contactId} !`);
+      return false;
+    }
+    console.log(contacts[index]);
+  } catch (err) {
+    console.log(`Error getting contact with id ${contactId}: `, err);
+    throw new Error(err);
   }
-  return index;
 };
 
 export const removeContact = async contactId => {
-  const contacts = await listContacts();
-  const index = await getContactById(contactId);
-  if (index && contacts) {
-    contacts.splice(index, 1);
-    await writeContactsFile(contacts);
-    console.log(`Contact with id ${contactId} removed successfully`);
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    if (index && contacts) {
+      contacts.splice(index, 1);
+      await writeContactsFile(contacts);
+      console.log(`Contact with id ${contactId} removed successfully`);
+    }
+  } catch (err) {
+    console.log(`Error removing contact with id ${contactId}: `, err);
+    throw new Error(err);
   }
 };
 
 export const addContact = async (name, email, phone) => {
-  const contacts = await listContacts();
-  if (contacts) {
-    const newContact = {
-      id: nanoid(),
-      name: name,
-      email: email,
-      phone: phone,
-    };
-    contacts.push(newContact);
-    await writeContactsFile(contacts);
-    console.log(
-      `Contact ${newContact.name} with email: ${newContact.email} and phone: ${newContact.phone} added successfully`
+  try {
+    const contacts = await listContacts();
+    if (contacts) {
+      const newContact = {
+        id: nanoid(),
+        name: name,
+        email: email,
+        phone: phone,
+      };
+      contacts.push(newContact);
+      await writeContactsFile(contacts);
+      console.log(
+        `Contact ${newContact.name} with email: ${newContact.email} and phone: ${newContact.phone} added successfully`
+      );
+    }
+  } catch (err) {
+    console.log('Error adding new contact: ', err);
+    throw new Error(err);
+  }
+};
+
+export const editContact = async contactId => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const question = prompt => {
+    return new Promise(resolve => {
+      rl.question(prompt, answer => {
+        resolve(answer);
+      });
+    });
+  };
+
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    const editContact = contacts[index];
+    console.log(`You are editing ${JSON.stringify(editContact, null, 2)}: `);
+
+    const answerName = await question(
+      `Put the new name (Hit enter to leave it as ${editContact.name}): `
     );
+    if (answerName) {
+      editContact.name = answerName;
+    }
+
+    const answerEmail = await question(
+      `Put the new e-mail (Hit enter to leave it as ${editContact.email}): `
+    );
+    if (answerEmail) {
+      editContact.email = answerEmail;
+    }
+
+    const answerPhone = await question(
+      `Put the new phone (Hit enter to leave it as ${editContact.phone}): `
+    );
+    if (answerPhone) {
+      editContact.phone = answerPhone;
+    }
+    contacts[index] = editContact;
+    console.log(`The new Contact is ${JSON.stringify(contacts[index], null, 2)}`);
+    await writeContactsFile(contacts);
+  } catch (error) {
+    console.error('An error occurred: ', error);
+  } finally {
+    rl.close();
   }
 };
 
